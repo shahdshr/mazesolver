25d350e5317cd4a8bb7a048f57e85aca05bef367
Shahd <chahdsahraoui04@gmail.com>
2026-09-12 22:50:45 +0100

    add motor movement debug

--- mazesolver.ino
+++ mazesolver.ino
@@ -306,6 +306,7 @@ void printMaze() {
 
 // ---------- Motor functions (Step 10 - real driver code) ----------
 void moveForward() {
+  Serial.println("Moving forward");
   Serial.println("ACTION: Move forward");
   digitalWrite(IN1, HIGH);
   digitalWrite(IN2, LOW);
